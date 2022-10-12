00db9ad09f3321d94f4ee03034b0d3813100cdb8
Kabir Kwatra <kabir@kwatra.me>
2022-10-11 17:11:16 -0700

    chore+feat(PATH): reorder paths and add dart pub

--- conf.d/user_path.fish
+++ conf.d/user_path.fish
@@ -4,10 +4,17 @@ set -gxa PATH $HOME/.local/bin
 if command -qs cargo
     set -gxa PATH $HOME/.cargo/bin
 end
+
 if command -qs deno
     set -gxa PATH $HOME/.deno/bin
+end
+
+if command -qs dart
+    set -gxa PATH $HOME/.pub-cache/bin
 end
 
 if command -qs emacs
     set -gxa PATH $HOME/.emacs.d/bin
 end
+
+set -gxa PATH /opt/depot_tools
